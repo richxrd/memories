a74e10f1886cab84b650240a4df0c65179e0eeb0
richxrd <linrichard98@gmail.com>
2022-05-02 14:51:08 -0400

    heroku setup

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
+app.get("/", (req, res) => {
+    res.send("App is running");
+});
+
 const PORT = process.env.PORT;
 
 mongoose
